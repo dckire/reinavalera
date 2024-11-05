4d8f6b4766612af7e79ac16cee0017b39701ac43
dckire <erickcamacho533@gmail.com>
2024-11-04 18:56:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+const buscador = document.getElementById('buscador');
+const botonBuscar = document.getElementById('boton-buscar');
+const resultados = document.getElementById('resultados');
+
+botonBuscar.addEventListener('click', () => {
+	const palabra = buscador.value.toLowerCase();
+	const enlaces = document.querySelectorAll('a');
+	const resultadosHtml = [];
+
+	enlaces.forEach((enlace) => {
+		const href = enlace.href;
+		const texto = enlace.textContent.toLowerCase();
+
+		if (texto.includes(palabra)) {
+			resultadosHtml.push(`<a href="${href}">${texto}</a>`);
+		}
+	});
+
+	resultados.innerHTML = resultadosHtml.join('');
+});
